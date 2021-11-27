8486e84199188ccbf65ec9fdc35ee6a5fc3dfa82
Krzysztof Cieslak <krzysztof_cieslak@windowslive.com>
2021-11-27 12:52:55 +0100

    Update sample

--- sample/sample.ts
+++ sample/sample.ts
@@ -1,10 +1,30 @@
-import { LsifReader, noopTransformer } from "../src/reader"
+import { LsifReader, UriTransformer } from "../src/reader"
+import { URI } from "vscode-uri"
 import * as fs from "fs"
+import * as path from "path"
 
 let r = new LsifReader()
 let content = fs.readFileSync("sample/sample.json").toString()
-r.load(content, () => noopTransformer)
-let uri = "file:///d:/Programowanie/Projekty/lsif-reader/src/reader.ts"
+
+let transformerFactory = (workspaceUri: string): UriTransformer => {
+    return {
+        toDatabase: (uri) => {
+            let wu = URI.parse(workspaceUri).fsPath
+            let joined = path.join(wu, uri).replace("\\", "/")
+            let nu = URI.file(joined).toString(true)
+            console.log("TO_DATABASE", workspaceUri, uri, nu)
+
+            return nu
+        },
+        fromDatabase: (uri) => {
+            console.log("FROM_DATABASE", workspaceUri, uri)
+            return uri
+        },
+    }
+}
+
+r.load(content, transformerFactory)
+let uri = "src/reader.ts"
 
 let result = r.documentSymbols(uri)
 console.log(result)
